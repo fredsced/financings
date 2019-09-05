2236e819be214ba2113dbd49b06ccc87008246bb
zd87 <daria.lecerf@gmail.com>
2019-09-05 15:19:08 +0200

    ContactRepo deleted

--- financings/src/main/java/fr/formation/financings/repositories/ContactRepo.java
+++
@@ -1,8 +0,0 @@
-package fr.formation.financings.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.formation.financings.entities.Contact;
-
-public interface ContactRepo extends JpaRepository<Contact, Long> {
-}
